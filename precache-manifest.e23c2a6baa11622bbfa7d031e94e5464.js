7e666ec4d41d1fd979c3577ad0c82fa550d1cf14
Maxim Nechiporuk <nechiporukmr@gmail.com>
2019-11-27 15:17:03 +0200

    Updates

---
+++ precache-manifest.e23c2a6baa11622bbfa7d031e94e5464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e91b7d9d2a0c29bab5972744889aad9",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "a499067131f63557b307",
+    "url": "/goit-react-hw-04-reader/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "fb1b30d996d56c092618",
+    "url": "/goit-react-hw-04-reader/static/css/main.3a38e0e2.chunk.css"
+  },
+  {
+    "revision": "a499067131f63557b307",
+    "url": "/goit-react-hw-04-reader/static/js/2.f071d0b0.chunk.js"
+  },
+  {
+    "revision": "fb1b30d996d56c092618",
+    "url": "/goit-react-hw-04-reader/static/js/main.acdd43e9.chunk.js"
+  },
+  {
+    "revision": "5f8e05dc0ae81b56fc8d",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.cd5c953e.js"
+  }
+]);
